5444e980832d6152d6760cb06cd9c65fe45a2886
Sanketpatil012 <122162948+Sanketpatil012@users.noreply.github.com>
2025-03-06 10:19:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+ let paragraphs = document.getElementById('counter')
+
+ let btn= document.getElementById('incrementBtn')
+
+btn.addEventListener('click' ,  ()=>{
+	  let count = parseInt(paragraphs.innerHTML) 
+	 alert(count)  
+	paragraphs.innerHTML= count +1
+})
